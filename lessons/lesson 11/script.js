9bbdf037d37caa8eefb1db044daef53ccdac68f4
Serg <serega101617@gmail.com>
2023-06-01 18:54:24 +0200

    first js

---
+++ lessons/lesson 11/script.js
@@ -0,0 +1,17 @@
+// alert('hello');
+// console.log('asdf');
+
+// const name = prompt('What is your name?');
+// console.log(name);
+
+// const userAge = 40;
+// userLogin = 'Starta Institute';
+// console.log(userAge);
+// console.log("userAge")
+
+const product = 'milk';
+const user = 'Bob';
+const price = 5;
+console.log(product);
+console.log(user);
+console.log(messpriceage);
