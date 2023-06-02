d7b2dbc8f5594446c31307658f52272b6139d6f4
Serg <serega101617@gmail.com>
2023-06-02 18:37:58 +0200

    added lesson 12 (js)

---
+++ lessons/lesson 12 JS + tasks +%/script.js
@@ -0,0 +1,59 @@
+// let number = 10;
+// let result = number + number + number + number;
+// let result = number * 4;
+
+// let number = 10;
+// let result = number ** 4;
+// console.log(result);
+
+// const P = 3.14;
+// let rad = prompt('Give your number', '');
+// let circle = P * rad ** 2;
+// console.log(circle);
+
+// let C = prompt('How many grade?', '');
+// let F = C * 1.8 + 32;
+// console.log(F);
+
+// let a = 454 / 24;
+// let b = 454%24;
+// console.log(`Plane come in ${a} days and ${b} hours`);
+
+// let cost = 1000;
+// let discount = 0.15;
+// console.log(cost / 1 * discount );
+
+// let order_cost = 1000;
+// let discount = 0.15;
+// console.log(order_cost - (order_cost / 1) * 0.15);
+
+// let price = 100;
+// let discount = 0.2;
+// let up_price = 10.5;
+// console.log(price * (1 - discount));
+// let price_disc = price / 100 * discount;
+// console.log(price - price_disc);
+// let margin = (price / 100) * up_price;
+// console.log(price + margin);
+
+// let a = +prompt('Number');
+// let b = +prompt('Number');
+// let c = +prompt('Number');
+// let res = a + b + c;
+// console.log(res / 3);
+
+// let num = +prompt('Number');
+// let x = num / 100;
+// console.log(num - x);
+// console.log(num * 0.99);
+// console.log(num - num * 0.01);
+
+// let order_cost = 3432;
+// let paid = 450;
+// let x = order_cost / 100;
+// let c = paid / x;
+// console.log(100 - c);
+
+// let order_cost = 3432;
+// let paid = 450;
+// console.log(100 - (paid / order_cost) * 100);
