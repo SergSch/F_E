0576a9c175355659d69933c87bbbc10614fcea20
Serg <serega101617@gmail.com>
2023-06-09 15:17:33 +0200

    added lesson 17, tasks

---
+++ lessons/lesson 17 Practice/script.js
@@ -0,0 +1,120 @@
+// const player1 = {
+//   name: 'Usein',
+//   health: 100,
+//   illnes: 90,
+// };
+// const player2 = {
+//   name: 'Mike',
+//   health: 70,
+//   illnes: 150,
+// };
+
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
+// alert(sum(10, 4));
+
+// Задача. Написать функцию, которая возвращает сумму четных чисел от 1 до 10
+// function sum() {
+//   let a = 0;
+//   for (let i = 0; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//       a += i;
+//     }
+//   }
+//   return a;
+// }
+// console.log(sum());
+
+// Задача. Написать функцию, которая принимает массив чисел (оценок студента) и возвращает его средний балл
+// function mittleGr(arr) {
+//   let a = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     a += arr[i];
+//   }
+//   return a / arr.length;
+// }
+// console.log(mittleGr([3, 4, 5]));
+
+// Задача. Написать функцию, которая массив игроков (объектов), со след/полями: имя_пользователя, ущерб и выводит среднее значение ущерба для них
+// const arr = [
+//   {
+//     name: 'Usein',
+//     health: 100,
+//     illnes: 90,
+//   },
+//   {
+//     name: 'Mike',
+//     health: 70,
+//     illnes: 150,
+//   },
+//   {
+//     name: 'Serg',
+//     health: 80,
+//     illnes: 50,
+//   },
+// ];
+// function averagePl(arr) {
+//   let a = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     a += arr[i].illnes;
+//   }
+//   return a / arr.length;
+// }
+// const finish = averagePl(arr);
+// console.log(`Средний ущерб игроков составляет ${finish}`);
+
+// Задача. Вывести информацию о пользователях
+// const users = [
+//   ['Ivan', 20, 'ivan@example.org'],
+//   ['Petr', 30, 'petr@example.org'],
+//   ['Pavel', 40, 'pavel@example.org'],
+// ];
+// function usersInfo() {
+//   console.log(`Name first user is ${users[0][0]}`);
+//   console.log(`Age first user is ${users[2][1]}`);
+//   console.log(`Email first user is ${users[1][2]}`);
+// }
+// usersInfo();
+
+// Задача. Написать функцию, которая применяет скидку (возвращает значение со скидкой)
+// function discount(order, disc) {
+//   let a = 100 - order / disc;
+//   return a;
+// }
+// let b = discount(100, 10);
+// console.log(b);
+
+// Задача. Написать функцию, которая получает товар (объект) с ценой в EUR и возвращает его стоимость (скаляр) в USD
+// const product = { title: 'Product 1', price: 500, quantity: 10 };
+// function currency_exchange() {
+//   return product.price * 0.9;
+// }
+// console.log(currency_exchange(product));
+
+// Задача. Написать функцию, которая принимает радиус и возвращает площадь окружности с указанным радиусом
+// function calcRad(rad) {
+//   return rad ** 2 * 3.14;
+// }
+// console.log(calcRad(5));
+
+// Задача. Напишите функцию, которая получает массив различных чисел (положительных и отрицательных) и возвращает массив отрицательных чисел
+// const arr = [4, 5, -3, -5, 8];
+// function choiseOdd(ar) {
+//   let a = [];
+//   for (let i = 0; i < ar.length; i++) {
+//     if (ar[i] < 0) {
+//       a.push(ar[i]);
+//     }
+//   }
+//   return a;
+// }
+// console.log(choiseOdd(arr));
+
+// Задача. Запросить у сотрудника (логичистической компании) ко-во часов полета транспортного средства (самолета) и вывести время, в которое он вернется назад
+// function planeCome() {
+//   let hours = +prompt('How long will fly the plane?');
+//   return hours % 24;
+// }
+// let time = planeCome();
+// console.log(`The plane come back at ${time} o'clock`);
